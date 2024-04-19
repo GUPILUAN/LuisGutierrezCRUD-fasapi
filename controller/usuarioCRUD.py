a46c398cf25605b847dc42a1b98755fc1690bf0f
Luis <gupiluan@hotmail.com>
2024-04-18 20:33:37 -0600

    Error solucionado en el metodo para actualizar y respuesta visual a errores

--- controller/usuarioCRUD.py
+++ controller/usuarioCRUD.py
@@ -10,7 +10,7 @@ router : APIRouter = APIRouter()
 async def createUsuario(usuario : Usuario) -> Usuario:
     existingUser : Usuario | None = await collection.find_one({"email" : usuario.email})
     if existingUser:
-        raise HTTPException(status_code = 404, detail = "El email ya existe")
+        raise HTTPException(status_code = 208, detail = "El email ya existe")
     result : results.InsertOneResult = await collection.insert_one(usuario.model_dump())
     usuario._id = str(result.inserted_id)
     return usuario
@@ -26,7 +26,7 @@ async def readUsuarios() -> list[Usuario]:
 #Obtiene un usuario por email y actualiza sus valores
 @router.put('/{email}', response_description="Actualizar usuario por email", response_model= Usuario)
 async def updateUsuario(email : str, userToUpdate : Usuario) -> Usuario:
-    updatedUser : Usuario | None = await collection.find_one_and_update({"email" : email}, {"set" : userToUpdate.model_dump()})
+    updatedUser : Usuario | None = await collection.find_one_and_update({"email" : email}, {"$set" : userToUpdate.model_dump()})
     if not updatedUser:
         raise HTTPException(status_code = 404, detail = "El usuario no existe")
     return updatedUser
@@ -46,3 +46,4 @@ async def deleteUsuario(email : str) -> Usuario:
     if not deletedUser:
         raise HTTPException(status_code = 404, detail = "El usuario no existe")
     return deletedUser
+
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from db.db import client
 from controller.usuarioCRUD import router as usuarios_router
 
 app = FastAPI()
 app.include_router(usuarios_router, tags=["usuarios"], prefix="/usuarios")
+
+# Handler para manejar excepciones y mostrarlas en pantalla
+@app.exception_handler(HTTPException)
+async def manejar_error_con_imagen(request : Request, exc :HTTPException ) -> HTMLResponse:
+    url_imagen : str = f"https://http.cat/{exc.status_code}.jpg"
+    return HTMLResponse(content=f"""
+        <html>
+            <head><title>Error {exc.status_code}</title></head>
+            <body>
+                <h1> Hubo un error :(</h1>
+                <img src="{url_imagen}" alt="Imagen de error">
+            </body>
+        </html>
+        """, status_code=exc.status_code)
+
 # MongoDB connection URL
 @app.on_event("shutdown")
 def shutdown_db_client():
     client.close()
+
+#Main para arrancar el server
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
